3a7aaac27a620080f87364327f228278ea076b23
gusalbukrk <gusalbukrk@gmail.com>
2024-02-07 14:43:37 -0300

    add `eslint` & `prettier` integration

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    root: true,
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: 6,
+        sourceType: "module"
+    },
+    plugins: [
+        "@typescript-eslint"
+    ],
+    rules: {
+        '@typescript-eslint/naming-convention': [
+            "warn",
+            {
+                selector: "import",
+                format: [ "camelCase", "PascalCase" ]
+            }
+        ],
+        '@typescript-eslint/semi': "warn",
+        curly: "warn",
+        eqeqeq: "warn",
+        'no-throw-literal': "warn",
+        semi: "off"
+    },
+    ignorePatterns: [
+        "out",
+        "dist",
+        "**/*.d.ts"
+    ],
+    extends: [
+        // sets up both `eslint-plugin-prettier` and `eslint-config-prettier`
+        // should be the last item in the `extends` array
+        "plugin:prettier/recommended",
+    ],
+};
